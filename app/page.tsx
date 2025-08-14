fa9c9acdfef6d33a185cfb7295470d8148acefd1
Abdullah Bin Safi <abdulahsafi9@gmail.com>
2025-08-14 20:41:14 +0600

    Nexify

---
+++ app/page.tsx
@@ -0,0 +1,286 @@
+'use client'
+
+import { useState, useRef } from "react";
+import { AnimatedBeamMultipleOutputDemo } from "@/components/demos/animated-beam-demo";
+import BoxRevealDemo from "@/components/demos/box-reveal-demo";
+import Navbar from "@/components/navbar";
+import SliderOne from "@/components/ui/slider";
+import { Spotlight } from "@/components/ui/spotlight";
+import Image from "next/image";
+import { ShootingStarsAndStarsBackgroundDemo } from "@/components/demos/shooting-stars-demo";
+import NumberTicker from "@/components/magicui/number-ticker";
+import { ScrollBasedVelocityDemo } from "@/components/demos/scroll-based-velocity-demo";
+import Link from "next/link";
+import BoxReveal from "@/components/magicui/box-reveal";
+import { AnimatedShinyTextDemo } from "@/components/demos/animated-shiny-text-demo";
+import WebsiteDesign from "./website-design";
+import GraphicDesign from "./graphic-design";
+import ShopifyStores from "./shopify-stores";
+import Brands from "./brands";
+import { WordPullUpDemo } from "@/components/demos/word-pull-up-demo";
+import Services from "./services";
+import { PiCheckBold } from "react-icons/pi";
+import FAQS from "./faq";
+import { InfiniteMovingLogos } from "@/components/ui/infinite-moving-logos";
+import { InfiniteMovingCardsDemo } from "./snippets/infinite-moving-card-snippet";
+
+export default function Home() {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+  const closeDropdown = () => {
+    setDropdownVisible(false);
+  };
+
+  const websiteDesignRef = useRef<HTMLDivElement>(null);
+  const graphicDesignRef = useRef<HTMLDivElement>(null);
+  const shopifyStoresRef = useRef<HTMLDivElement>(null);
+  const brandsRef = useRef<HTMLDivElement>(null);
+  const servicesRef = useRef<HTMLDivElement>(null);
+
+  const scrollToWebsiteDesign = () => {
+    websiteDesignRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
+  };
+
+  const scrollToGraphicDesign = () => {
+    graphicDesignRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const scrollToShopifyStores = () => {
+    shopifyStoresRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const scrollToBrands = () => {
+    brandsRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  // Function to scroll to Services section
+  const scrollToServices = () => {
+    servicesRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const services = [
+  {
+    icon: "/images/s_6.png",
+    title: "Web Design + Development",
+    description:
+      "Take your business to the next level with our web design and development services",
+  },
+  {
+    icon: "/images/s_1.png",
+    title: "Search Engine Optimization",
+    description:
+      "Get your website to the top of search engine results with our SEO services",
+  },
+  {
+    icon: "/images/s_5.png",
+    title: "Content Creation",
+    description:
+      "Boost your brand's online presence with our social media marketing services",
+  },
+  {
+    icon: "/images/s_3.png",
+    title: "Social Media Marketing",
+    description:
+      "Interact with your customers and increase sales with our email marketing services",
+  },
+  {
+    icon: "/images/s_4.png",
+    title: "Email Marketing",
+    description:
+      "With our content creation services, we help businesses drive results",
+  },
+  {
+    icon: "/images/s_2.png",
+    title: "Pay-Per-Click Advertising",
+    description:
+      "Don't waste money on ineffective advertising. Our PPC services help you reach your target audience",
+  },
+];
+
+  return (
+    <div className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Navbar
+        scrollToWebsiteDesign={scrollToWebsiteDesign}
+        scrollToGraphicDesign={scrollToGraphicDesign}
+        scrollToShopifyStores={scrollToShopifyStores}
+        scrollToBrands={scrollToBrands}
+        scrollToServices={scrollToServices}
+      />
+
+      <Spotlight className="hidden md:flex md:-top-80 left-80  " fill="white" />
+      <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
+              <AnimatedShinyTextDemo />
+        <div className="text-4xl pb-5 md:text-7xl px-6 text-center  bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg-neutral-400 bg-opacity-50">
+          Create, grow, and <br /> scale your business
+        </div>
+        <p className="mt-4 text-lg font-normal  text-neutral-300 max-w-lg text-center mx-auto px-4">
+          Custom tailored solutions for your business. We are a team of creatives who are excited to help you grow your business.
+        </p>
+
+        <Link
+          href={"/book"}
+          className="cursor-pointer flex items-center justify-center border rounded-full w-48 p-2  mx-auto my-6 text-white "
+        >
+          Book a call
+        </Link>
+
+        <div className="w-full pt-20">
+          <SliderOne />
+        </div>
+
+        
+             <div className="grid grid-cols-2 bg-gradient-to-b from-neutral-50
+              to bg-neutral-400 bg-opacity-50 md:grid-cols-4 gap-4 items-center text-left md:justify-items-center md:mx-auto mt-10 md:mt-16">
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Design
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Development
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Marketing
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5} >
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Strategy
+              </p>
+            </BoxReveal>
+          </div>
+
+
+  <div className="md:flex items-center bg-white justify-between gap-y-4 my-10 gap-x-28 mx-auto">
+            <div className="md:w-2/5">
+              <h1 className="text-2xl ml-[20px] font-medium text-gray-600 w-4/5">
+                Trusted by fast moving brands worldwide
+              </h1>
+
+              <div className="flex ml-[20px] my-6 gap-x-5 w-full">
+                <div>
+                  <h1 className="text-blue-500 text-3xl md:text-5xl">
+                    <NumberTicker value={100} /> +
+                    <p className="text-gray-500 text-sm md:text-md">
+                      Happy Clients
+                    </p>
+                  </h1>
+                </div>
+
+                <div className="w-px bg-gray-300 self-stretch"></div>
+
+                <div className="flex-1 min-w-0">
+                  <h1 className="text-blue-500 text-3xl md:text-5xl whitespace-nowrap overflow-hidden">
+                    <NumberTicker value={50} /> +
+                    <p className="text-gray-500 text-sm md:text-md">
+                      Projects Completed
+                    </p>
+                  </h1>
+                </div>
+              </div>
+            </div>
+
+            <section className="overflow-hidden mt-10 md:w-4/5">
+              <InfiniteMovingLogos
+                speed="slow"
+                direction="left"
+                items={[
+                  {
+                    logo: "/logo/logo.png",
+                    name: "Logo",
+                  },
+                  {
+                    logo: "/logo/logo.png",
+                    name: "Logo",
+                  },
+                ]}
+              />
+            </section>
+          </div>
+   <div id ='services'>
+        <Services />
+        </div>
+
+
+  <section className="py-20">
+        <ScrollBasedVelocityDemo />
+      </section>
+
+
+            <main className="md:px-0 mx-6 md:mx-auto">
+        <h1 className="text-3xl text-gray-500  md:text-5xl md:text-center font-medium flex items-center gap-x-2 mx-auto justify-center">
+        Our{" "}
+            <span className="text-blue-500 flex gap-x-1 items-center">
+              {" "}
+              <Image
+                src={"/icons/squiggle.svg"}
+                width={10000}
+                height={10000}
+                className="w-6"
+                alt="image"
+              />
+              Creative
+              <Image
+                src={"/icons/star.svg"}
+                width={10000}
+                height={10000}
+                className="w-6 mb-8"
+                alt="image"
+              />
+            </span>{" "}
+            Process
+          </h1>
+
+          <p className="text-center 
+          py-4 md:w-1/2 mx-auto 
+          text-xl md:text-2xl text-gray-500">
+            All of our services are designed to help your business to get
+            noticed.
+          </p>
+
+          <div className="flex flex-col md:flex-row items-center justify-center w-full md:w-1/2 mx-auto">
+          <div className="w-full md:w-1/2 order-2 md:order-1">
+            <AnimatedBeamMultipleOutputDemo />
+          </div>
+          <div className="w-full md:w-1/2 order-1 md:order-2 md:ml-0">
+            <BoxRevealDemo />
+          </div>
+
+          </div>
+
+        </main>  
+
+        <div ref={websiteDesignRef}>
+          <WebsiteDesign />
+        </div>
+        <div ref={graphicDesignRef}>
+          <GraphicDesign />
+        </div>
+       
+        <div ref={brandsRef}>
+          <Brands />
+        </div>
+     
+         <ShootingStarsAndStarsBackgroundDemo />
+
+        <InfiniteMovingCardsDemo />
+
+              
+        <FAQS />
+      </div>
+    </div>
+  );
+}
