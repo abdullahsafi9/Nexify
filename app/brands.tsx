007bd484e04a07a717e9c99ce8f6578cb6b63060
Abdullah Bin Safi <abdulahsafi9@gmail.com>
2025-08-17 17:24:42 +0600

    Update brands.tsx

--- app/brands.tsx
+++ app/brands.tsx
@@ -61,7 +61,7 @@ const Brands = () => {
 
  <Link
           href={"/book"}
-          className="cursor-pointer mt-[-20px] flex items-center justify-center border rounded-full w-48 p-2  mx-auto my-6 text-white "
+          className="cursor-pointer md:mt-[20px] flex items-center justify-center border rounded-full w-48 p-2  mx-auto my-6 text-white "
         >
           Learn More
         </Link>
