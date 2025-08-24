781e0dcbe3589308474cad32ade1ddd7c9215601
Abdullah Bin Safi <abdulahsafi9@gmail.com>
2025-08-24 19:24:54 +0600

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,32 +1,130 @@
+'use client'
+
+import { useState, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-const Footer = () => {
-  return (
-    <footer className="bg-[#fafafa] rounded-[20px] mt-[30px] py-10  px-6 md:px-0 md:mx-auto border-t">
-      <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
-        <h1 className="text-3xl md:text-5xl font-medium ">
-          <Image
-            src={"/logo/logo.png"}
-            width={10000}
-            height={10000}
-            className="w-40"
-            alt="image"
-          />{" "}
-        </h1>
-        <p className="text-left  text-xl  text-gray-500">+88 016 188 2472</p>
-        <p className="text-left  text-xl  text-gray-500">
-          admin@birdsoftware.ca
-        </p>
-      </div>
 
-      <div className="flex md:justify-center gap-x-4 mt-10">
-        © 2024 Bird. All Rights Reserved.
-        <Link href="/" className="text-blue-500">
-          Privacy Policy
-        </Link>
+
+const Footer = () => {
+   
+  
+  return (
+    <footer className="bg-[#fafafa] rounded-[20px] mt-[30px]">
+    <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 mt-[-20px] md:mb-0">
+              <Link href="/" className="flex items-center">
+                  <img src="/logo/logo.png" className="w-40" alt="FlowBite Logo" />
+                 
+              </Link>
+                <form 
+                        onSubmit={(e) => e.preventDefault()}
+                    >
+                        <label className="block mt-[-40px] ml-[25px] pt-4 pb-2">
+                            Stay up to date
+                        </label>
+                        <div className="max-w-sm flex  ml-[23px] items-center border rounded-md p-1">
+                            <input 
+                                type="email"
+                                placeholder="Enter your email"
+                                className="w-full p-2.5 outline-none"
+                            />
+                            <button
+                                className="p-2.5 rounded-md text-white bg-indigo-600 outline-none shadow-md focus:shadow-none sm:px-5"
+                            >
+                                Subscribe
+                            </button>
+                        </div>
+                    </form>
+          </div>
+          <div className="grid grid-cols-2 gap-4 sm:gap-6 sm:grid-cols-3">
+              <div>
+                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                      <li >
+                          <a href="/website" className="hover:underline">Websites</a>
+                      </li>
+                      <li>
+                          <a href="/website-design" className="hover:underline">Website Design</a>
+                      </li>
+                       <li>
+                          <a href="/graphic-design" className="hover:underline">Graphic Design</a>
+                      </li>
+                       <li>
+                          <a href="/brand" className="hover:underline">Brands</a>
+                      </li>
+                       <li>
+                          <a href="/pricing" className="hover:underline">Pricing</a>
+                      </li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Address</h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                      <li className="mb-4">
+                          <a href="https://www.facebook.com/profile.php?id=61576740625629" className="hover:underline text:sm">Head Office: <br></br>14, Gausul Azam Avenue Road, <br></br>Sector 14, Uttara, Dhaka -1230</a>
+                      </li>
+                      <li>
+                          <a href="https://www.youtube.com/@NexifyLimited-x8i2f" className="hover:underline">Corporate Office: <br></br>6th Floor, K C Tower, <br></br> Sonargaon Janapath Road, <br></br>Sector 13, Uttara, Dhaka -1230</a>
+                      </li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                      <li className="mb-4">
+                          <a href="#" className="hover:underline">Privacy Policy</a>
+                      </li>
+                      <li>
+                          <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
       </div>
-    </footer>
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="/" className="hover:underline">Nexify Limited™</a>. All Rights Reserved.
+          </span>
+            <h2 className="ml-[540px] text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+          <div className="flex mt-4 sm:justify-center sm:mt-0">
+           
+              <a href="https://www.facebook.com/profile.php?id=61576740625629" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                  <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
+                        <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd"/>
+                    </svg>
+                  <span className="sr-only">Facebook page</span>
+              </a>
+              <a href="https://www.youtube.com/@NexifyLimited-x8i2f" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+                  <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
+                        <path fill-rule="evenodd"  d="M18.168 19.0028C20.4724 19.0867 22.41 17.29 22.5 14.9858V9.01982C22.41 6.71569 20.4724 4.91893 18.168 5.00282H6.832C4.52763 4.91893 2.58998 6.71569 2.5 9.01982V14.9858C2.58998 17.29 4.52763 19.0867 6.832 19.0028H18.168Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M12.008 9.17784L15.169 11.3258C15.3738 11.4454 15.4997 11.6647 15.4997 11.9018C15.4997 12.139 15.3738 12.3583 15.169 12.4778L12.008 14.8278C11.408 15.2348 10.5 14.8878 10.5 14.2518V9.75184C10.5 9.11884 11.409 8.77084 12.008 9.17784Z" clipRule="evenodd"/>
+                    </svg>
+                  <span className="sr-only">Youtube</span>
+              </a>
+              
+              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+                  <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
+                    <path fill-rule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clipRule="evenodd"/>
+                </svg>
+                  <span className="sr-only">Twitter page</span>
+              </a>
+              <a href="https://github.com/limitednexify" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+                  <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clipRule="evenodd"/>
+                  </svg>
+                  <span className="sr-only">GitHub account</span>
+              </a>
+              <a href="https://www.instagram.com/limitednexify/" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+                  <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd" d="M24 4.322c6.413 0 7.172.028 9.694.14 2.343.104 3.61.497 4.453.825 1.116.432 1.922.957 2.756 1.791.844.844 1.36 1.64 1.79 2.756.329.844.723 2.12.826 4.454.112 2.53.14 3.29.14 9.693 0 6.413-.028 7.172-.14 9.694-.103 2.344-.497 3.61-.825 4.453-.431 1.116-.957 1.922-1.79 2.756-.845.844-1.642 1.36-2.757 1.791-.844.328-2.119.722-4.453.825-2.532.112-3.29.14-9.694.14-6.413 0-7.172-.028-9.694-.14-2.343-.103-3.61-.497-4.453-.825-1.115-.431-1.922-.956-2.756-1.79-.844-.844-1.36-1.641-1.79-2.757-.329-.844-.723-2.119-.826-4.453-.112-2.531-.14-3.29-.14-9.694 0-6.412.028-7.172.14-9.694.103-2.343.497-3.609.825-4.453.431-1.115.957-1.921 1.79-2.756.845-.844 1.642-1.36 2.757-1.79.844-.329 2.119-.722 4.453-.825 2.522-.113 3.281-.141 9.694-.141zM24 0c-6.516 0-7.331.028-9.89.14-2.55.113-4.304.526-5.822 1.116-1.585.619-2.926 1.435-4.257 2.775-1.34 1.332-2.156 2.672-2.775 4.247C.666 9.806.253 11.55.141 14.1.028 16.669 0 17.484 0 24s.028 7.331.14 9.89c.113 2.55.526 4.304 1.116 5.822.619 1.585 1.435 2.925 2.775 4.257a11.732 11.732 0 004.247 2.765c1.528.591 3.272 1.003 5.822 1.116 2.56.112 3.375.14 9.89.14 6.516 0 7.332-.028 9.891-.14 2.55-.113 4.303-.525 5.822-1.116a11.732 11.732 0 004.247-2.765 11.732 11.732 0 002.766-4.247c.59-1.528 1.003-3.272 1.115-5.822.113-2.56.14-3.375.14-9.89 0-6.516-.027-7.332-.14-9.891-.112-2.55-.525-4.303-1.115-5.822-.591-1.594-1.407-2.935-2.747-4.266a11.732 11.732 0 00-4.247-2.765C38.194.675 36.45.262 33.9.15 31.331.028 30.516 0 24 0z" clipRule="evenodd"/><path d="M24 11.672c-6.806 0-12.328 5.522-12.328 12.328 0 6.806 5.522 12.328 12.328 12.328 6.806 0 12.328-5.522 12.328-12.328 0-6.806-5.522-12.328-12.328-12.328zm0 20.325a7.998 7.998 0 010-15.994 7.998 7.998 0 010 15.994zM39.694 11.184a2.879 2.879 0 11-2.878-2.878 2.885 2.885 0 012.878 2.878z"/>
+                </svg>
+                  <span className="sr-only">Instagram account</span>
+              </a>
+          </div>
+      </div>
+    </div>
+</footer>
   );
 };
 
